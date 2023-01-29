61d6aa6b9871117e51610ccb0633c04a675c8033
Carlo <carlo@Ubuntu.myguest.virtualbox.org>
2023-01-29 19:42:07 +0100

    Aggiornamento file main.c

--- main.c
+++ main.c
@@ -1,16 +1,33 @@
-// build with "gcc main.c game.c menu.c appearance.c -lncurses -o dino.exe"
-
+// build with "gcc main.c game.c menu.c appearance.c -lncurses -o dino.exe
+/** 
+	\file main.c
+	\author Pognant Carlo
+	\version 1.0
+	\date 2023-01-29
+*/
 #include <ncurses.h>
 #include <stdio.h>
 #include "menu.h"
 #include "minunit.h"
+
+/**
+	\fn int main(int argc, char **argv)
+	\brief Funzione principale del programma
+	Questa funzione gestisce l'avvio del programma e la sua modalità di esecuzione
+	Se viene specificato il comando '-test', verranno eseguiti tutti i test del programma 		
+	utilizzando la libreria minunit.h
+	Altrimenti verrà avviato il menu principale del gioco usando la libreria ncurses
+	\param argc Numero di argomenti passati al programma
+	\param argv Array di argomenti pasati al programma
+	\return int: Fa tornare o 1 se il gioco non è terminato oppure 0 se il gioco è terminato
+*/
 
 int main(int argc, char **argv) {
 	// Run tests (if the -test command argument has been specified)
 	int testsResult = run_all_tests(argc, argv);
 	if(testsResult > 0)
 		return testsResult != 0;
-
+	
 	// Normal star
 	initscr();
 	start_color();
